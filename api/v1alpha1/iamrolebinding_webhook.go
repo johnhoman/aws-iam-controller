056cc8884b51b94e02eeec134acd2f91a5b29d00
Jack Homan <homan.jack@gmail.com>
2022-03-25 23:01:00 -0400

    remove scaffolded todos

--- api/v1alpha1/iamrolebinding_webhook.go
+++ api/v1alpha1/iamrolebinding_webhook.go
@@ -32,7 +32,6 @@ func (r *IamRoleBinding) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		Complete()
 }
 
-// TODO(user): EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
 //+kubebuilder:webhook:path=/mutate-aws-jackhoman-com-v1alpha1-iamrolebinding,mutating=true,failurePolicy=fail,sideEffects=None,groups=aws.jackhoman.com,resources=iamrolebindings,verbs=create;update,versions=v1alpha1,name=miamrolebinding.kb.io,admissionReviewVersions=v1
 
@@ -42,10 +41,8 @@ var _ webhook.Defaulter = &IamRoleBinding{}
 func (r *IamRoleBinding) Default() {
 	iamrolebindinglog.Info("default", "name", r.Name)
 
-	// TODO(user): fill in your defaulting logic.
 }
 
-// TODO(user): change verbs to "verbs=create;update;delete" if you want to enable deletion validation.
 //+kubebuilder:webhook:path=/validate-aws-jackhoman-com-v1alpha1-iamrolebinding,mutating=false,failurePolicy=fail,sideEffects=None,groups=aws.jackhoman.com,resources=iamrolebindings,verbs=create;update,versions=v1alpha1,name=viamrolebinding.kb.io,admissionReviewVersions=v1
 
 var _ webhook.Validator = &IamRoleBinding{}
@@ -53,23 +50,17 @@ var _ webhook.Validator = &IamRoleBinding{}
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *IamRoleBinding) ValidateCreate() error {
 	iamrolebindinglog.Info("validate create", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object creation.
 	return nil
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *IamRoleBinding) ValidateUpdate(old runtime.Object) error {
 	iamrolebindinglog.Info("validate update", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object update.
 	return nil
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *IamRoleBinding) ValidateDelete() error {
 	iamrolebindinglog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
 }
